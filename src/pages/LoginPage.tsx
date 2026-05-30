581703fa4698d8cc109f3d7fca1680fdb68de4a0
RomanFronrEnd <romariotraveler@gmail.com>
2026-05-30 23:12:03 +0300

    add favicon & create login & register pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+
 const LoginPage = () => {
-	return <h1>Login Page</h1>;
+	const [email, setEmail] = useState('');
+
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		console.log(email);
+
+		console.log(password);
+	};
+
+	return (
+		<section>
+			<h1>Login</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type='email'
+					placeholder='Email'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+
+				<input
+					type='password'
+					placeholder='Password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+
+				<button type='submit'>Login</button>
+			</form>
+		</section>
+	);
 };
 
 export default LoginPage;
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,62 @@
+import { useState } from 'react';
+
 const RegisterPage = () => {
-	return <h1>Register Page</h1>;
+	const [name, setName] = useState('');
+
+	const [email, setEmail] = useState('');
+
+	const [password, setPassword] = useState('');
+
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		console.log({
+			name,
+			email,
+			password,
+			confirmPassword,
+		});
+	};
+
+	return (
+		<section>
+			<h1>Register</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='Name'
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+
+				<input
+					type='email'
+					placeholder='Email'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+
+				<input
+					type='password'
+					placeholder='Password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+
+				<input
+					type='password'
+					placeholder='Confirm Password'
+					value={confirmPassword}
+					onChange={(e) => setConfirmPassword(e.target.value)}
+				/>
+
+				<button type='submit'>Register</button>
+			</form>
+		</section>
+	);
 };
 
 export default RegisterPage;
